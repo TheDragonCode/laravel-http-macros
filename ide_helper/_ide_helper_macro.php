b53671dcb9c235bbdeda61482b53dfb3c8be56e9
Andrey Helldar <helldar@dragon-code.pro>
2024-08-23 11:43:25 +0300

    Fixed code-style

--- ide_helper/_ide_helper_macro.php
+++ ide_helper/_ide_helper_macro.php
@@ -1,8 +1,8 @@
 <?php
+
 /** @noinspection all */
 
 namespace Illuminate\Http\Client {
-    
     /**
      * @method $this toData(\Closure|string $class, int|string|null $key = null)
      * @method $this toDataCollection(\Closure|string $class, int|string|null $key = null)
